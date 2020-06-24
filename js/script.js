b34626bf5dd3c7a70f401cd1e442f46c083379ee
Sara <saracorsob@gmail.com>
2020-06-24 16:17:07 +0200

    Creazione array di oggetti, creato ciclo e stampato nomi e cognomi

--- js/script.js
+++ js/script.js
@@ -7,5 +7,31 @@ var studente = {
 // console.log(studente);
 
 for (var chiave in studente ) {
-  console.log(studente[chiave]);
+  // console.log(studente[chiave]);
+}
+
+var arrayStudenti = [
+  {
+    "nome": "Carlo",
+    "cognome": "Verdi",
+    "eta": 20,
+  },
+  {
+    "nome": "Alessio",
+    "cognome": "Bianchi",
+    "eta": 22,
+  },
+  {
+    "nome": "Lorenzo",
+    "cognome": "Neri",
+    "eta": 21,
+  }
+];
+
+for (var i = 0; i < arrayStudenti.length; i++) {
+  // console.log(arrayStudenti[i]);
+  var singoloStudente = arrayStudenti[i];
+
+  console.log(singoloStudente.nome);
+  console.log(singoloStudente.cognome);
 }
